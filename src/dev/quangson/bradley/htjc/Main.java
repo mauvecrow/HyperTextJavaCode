fff79f5f4a1801722fe646b055be354f40f65c6f
Bradley Quangson <bradley.quangson@gmail.com>
2025-02-19 10:29:04 -0700

    Initial Commit

---
+++ src/dev/quangson/bradley/htjc/Main.java
@@ -0,0 +1,8 @@
+package dev.quangson.bradley.htjc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome ot HTJC - Hyper Text Java Code!");
+    }
+}
