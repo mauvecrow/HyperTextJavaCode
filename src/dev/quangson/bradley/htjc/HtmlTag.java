d5fbb9f2ccf410f4d1f820c033cb0536ddbb9b0f
Bradley Quangson <bradley.quangson@gmail.com>
2025-02-19 10:52:41 -0700

    HtmlTag -- enum to represent html tag. currenlty defined enough for basic tags vcs.xml - part of intellij startup

---
+++ src/dev/quangson/bradley/htjc/HtmlTag.java
@@ -0,0 +1,46 @@
+package dev.quangson.bradley.htjc;
+
+public enum HtmlTag {
+
+        HTML("html"), HEAD("head"),	BODY("body"), TITLE("title"),
+        H1("h1"), H2("h2"),	H3("h3"),H4("h4"), H5("h5"),	H6("h6"),
+        P("p"), DIV("div"),	SPAN("span"),
+        UL("ul"), OL("ol"),	LI("li"),
+        TABLE("table"),	TR("tr"), TD("td"),	TH("th"),
+        IMG("img", true),
+        A("a"), LINK("link", true),
+        FORM("form"), INPUT("input", true), LABEL("label"), BUTTON("button"),	TEXTAREA("textarea"),
+        I("i"),	U("u"), EM("em"), STRONG("strong"),
+        BR("br", true);
+
+        private final String tag;
+        private final boolean selfClosing;
+
+        HtmlTag(String string) {
+            tag = string;
+            selfClosing = false;
+        }
+
+        HtmlTag(String tag, boolean flag){
+            this.tag = tag;
+            selfClosing = flag;
+        }
+
+        public String getTag() {
+            return tag;
+        }
+
+
+
+        public boolean isSelfClosing() {
+            return selfClosing;
+        }
+
+        public String startTag() {
+            return !selfClosing ? "<" + tag + ">" : "<" + tag + "/>";
+        }
+
+        public String endTag() {
+            return !selfClosing ? "</" + tag + ">" : "<" + tag + "/>";
+        }
+}
