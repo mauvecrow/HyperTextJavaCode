6bdef31b730e2adac8584dc9fdf3ad2ff5810b80
Bradley Quangson <bradley.quangson@gmail.com>
2025-02-20 00:13:31 -0700

    HtmlTag - created new empty tag Updated tag methods so that no brackets are rendered. This empty tag is used to insert plain text without a tag. Useful if nesting text with semantic/phrasing tags

--- src/dev/quangson/bradley/htjc/HtmlTag.java
+++ src/dev/quangson/bradley/htjc/HtmlTag.java
@@ -6,12 +6,13 @@ public enum HtmlTag {
         H1("h1"), H2("h2"),	H3("h3"),H4("h4"), H5("h5"),	H6("h6"),
         P("p"), DIV("div"),	SPAN("span"),
         UL("ul"), OL("ol"),	LI("li"),
-        TABLE("table"),	TR("tr"), TD("td"),	TH("th"),
+        TABLE("table"),	TR("tr"), TD("td"),	TH("th"), THEAD("th"), TFOOT("tf"), CAPTION("caption"),
         IMG("img", true),
         A("a"), LINK("link", true),
         FORM("form"), INPUT("input", true), LABEL("label"), BUTTON("button"),	TEXTAREA("textarea"),
         I("i"),	U("u"), EM("em"), STRONG("strong"),
-        BR("br", true);
+        BR("br", true),
+        empty("empty", true);
 
         private final String tag;
         private final boolean selfClosing;
@@ -26,29 +27,16 @@ public enum HtmlTag {
             selfClosing = flag;
         }
 
-        public String getTag() {
-            return tag;
-        }
-
-
-
-        public boolean isSelfClosing() {
-            return selfClosing;
-        }
-
-        public String startTag() {
-            return !selfClosing ? "<" + tag + ">" : "<" + tag + "/>";
-        }
 
         public String endTag() {
             return !selfClosing ? "</" + tag + ">" : "";
         }
 
         public String openStartTag(){
-            return "<" + tag + " ";
+            return !tag.equals("empty") ? "<" + tag + " " : "";
         }
 
         public String closeStartTag(){
-            return !selfClosing ? ">" : "/>";
+            return !tag.equals("empty") ? !selfClosing ? ">" : "/>" : "";
     }
 }
